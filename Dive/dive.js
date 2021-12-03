659e2f8b12e0e37ab8eeccb4d6b017ddb9fee3dc
Edouard Souan <edouard.souan@eshard.com>
2021-12-03 09:19:19 +0100

    Day 2

---
+++ Dive/dive.js
@@ -0,0 +1,38 @@
+// TODO: Get the final horizontal position and depth 
+// then multiply it and submit the result
+
+const fs = require('fs')
+const { mainModule } = require('process')
+
+function getFinalPosition(commands){
+    let arrival = { horizontal: 0, depth: 0};
+
+    for(const command of commands){
+        const [direction, value] = command.split(' ')
+        const numericValue = parseInt(value)
+        switch(direction){
+            case "forward":
+                arrival.horizontal += numericValue;
+                break;
+            case "up":
+                arrival.depth -= numericValue;
+                break;
+            case "down":
+                arrival.depth += numericValue;
+                break;
+        }
+    }
+    return arrival
+}
+
+function main(){
+    console.log("START")
+    const data = fs.readFileSync("./input.txt", "utf-8");
+    const commands = data.split("\n");
+    const arrival = getFinalPosition(commands);
+
+    console.log(`Horizontal x Depth = ${arrival.horizontal * arrival.depth}`)
+    console.log("END")
+}
+
+main()
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # advent-of-code-2021
 
 ## DAY 1: [Sonar Sweep](https://adventofcode.com/2021/day/1)
+
+## DAY 2: [Dive!](https://adventofcode.com/2021/day/2)
